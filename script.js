122432e2d9a443a6a127f74ce1cab2b1a2330bfd
Farai Major <faraimutukumira@gmail.com>
2022-01-11 12:32:09 -0500

    closes #7, closes #8, closes #9, closes #10, closes #11, Added colored to cells

--- script.js
+++ script.js
@@ -1,39 +1,102 @@
-let table = document.getElementById('myTable');
+let columns = 1;
+let mouseOver = false;
 
 function addRow() {
-    if (table.rows.length === 0) {
-        let addRow = table.insertRow(0);
-        let cell = addRow.insertCell(0)
-    } else {
-        let addRow = table.insertRow(0);
-        let e = table.rows.length - 1;
-        let len = table.rows[e].cells.length;
-
-        for (let i = 0; i < len; i++) {
-            table.rows[0].insertCell(i);
-            table.rows[0].cells[i].innerHTML = "";
-        }
+    let table = document.getElementById("myTable");
+    let row = document.createElement("tr");
+    row.classList.add("row");
+    table.appendChild(row);
+    for (let i = 0; i < columns; i++) {
+        let cell = document.createElement("td");
+        cell.classList.add("cell");
+        row.appendChild(cell);
     }
 }
+
+
+function deleteRow() {
+    let row = document.getElementsByClassName("row");
+    let tr = Array.from(row);
+    tr[tr.length - 1].parentNode.removeChild(tr[tr.length - 1]);
+}
+
 
 
 function addColumn() {
-    if (table.rows.length === 0) {
-        addRow();
-    } else {
-        for (let i = 0; i < table.rows.length; i++) {
-            table.rows[i].insertCell(0);
-            table.rows[i].cells[0].innerHTML = "";
+    let row = document.getElementsByClassName("row");
+    let tr = Array.from(row);
+    for (let i = 0; i < tr.length; i++) {
+        let cell = document.createElement("td");
+        cell.classList.add("cell");
+        tr[i].appendChild(cell);
+    }
+    columns++;
+}
+
+
+function deleteColumn() {
+    let row = document.getElementsByClassName("row");
+    let tr = Array.from(row);
+    if (tr.length == 0) {
+        alert('Error: there is nothing to remove');
+        return this;
+    }
+    for (let i = 0; i < tr.length; i++) {
+        tr[i].removeChild(tr[i].lastChild);
+    }
+    columns--;
+}
+
+
+
+function fillUncolored() {
+    let color = document.querySelector("#color").value
+    let allCells = document.getElementsByTagName("td");
+    for (let i = 0; i < allCells.length; i++) {
+        if (allCells[i].style.backgroundColor == "") {
+            allCells[i].style.backgroundColor = color;
         }
     }
 }
 
-function deleteRow() {
-    document.getElementById("myTable").deleteRow(0);
+
+function fillAll() {
+    let color = document.querySelector("#color").value
+    let allCells = document.getElementsByTagName("td");
+    for (let i = 0; i < allCells.length; i++) {
+        allCells[i].style.backgroundColor = color;
+    }
 }
 
-function deleteColumn() {
-    for (let r = 0; r < table.rows.length; r++) {
-        table.rows[r].deleteCell(0); // var table handle
+
+function clearAll() {
+    let allCells = document.getElementsByTagName("td");
+    for (let i = 0; i < allCells.length; i++) {
+        allCells[i].style.backgroundColor = "";
     }
+}
+
+
+function colorChange(cell) {
+    let color = document.querySelector("#color").value
+    cell.addEventListener("click", function() {
+        cell.style.backgroundColor = color;
+    })
+
+    cell.addEventListener("mousedown", function() {
+        mouseOver = true;
+        cell.style.backgroundColor = color;
+    })
+
+    cell.addEventListener("mousemove", function() {
+        if (mousOver == true) {
+            cell.style.backgroundColor = color;
+        }
+    })
+
+    cell.addEventListener("mouseup", function() {
+        if (mouseOver == true) {
+            mouseOver = false;
+        }
+    })
 }
